cecc2aeda78552e2911e3ed37a4285dbe81448c9
jhnyuk <debussysuh@gmail.com>
2024-04-14 16:36:35 +0900

    first settings

---
+++ src/main/java/com/igdy/igeodaeyeo/IgeodaeyeoApplication.java
@@ -0,0 +1,13 @@
+package com.igdy.igeodaeyeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgeodaeyeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgeodaeyeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igdy/igeodaeyeo/IgeodaeyeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igdy.igeodaeyeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgeodaeyeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
