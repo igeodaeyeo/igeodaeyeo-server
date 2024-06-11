4fa7a46efdd71c94e8744102dcd7e1f58c0468c0
sungjin122517 <sungjin122517@naver.com>
2024-06-11 16:30:32 +0800

    user entity 수정

---
+++ src/main/java/com/igdy/igeodaeyeo/domain/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.igdy.igeodaeyeo.domain.user.entity;
+
+public enum Role {
+    ADMIN, USER;
+}
--- src/main/java/com/igdy/igeodaeyeo/domain/user/entity/User.java
+++ src/main/java/com/igdy/igeodaeyeo/domain/user/entity/User.java
@@ -1,11 +1,16 @@
 package com.igdy.igeodaeyeo.domain.user.entity;
 
+import com.igdy.igeodaeyeo.global.security.OAuthProvider;
 import com.igdy.igeodaeyeo.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Getter
 @Entity
 @NoArgsConstructor
 @SuperBuilder
@@ -19,4 +24,15 @@ public class User extends BaseEntity {
     private String profileImageUrl;
     private String birth;
     private String sex;
+//    private String email;
+    private OAuthProvider oAuthProvider;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public User(String email, String nickname, OAuthProvider oAuthProvider) {
+        this.loginId = email;
+        this.nickname = nickname;
+        this.oAuthProvider = oAuthProvider;
+    }
 }
