0eee108094fab950ca3e0d9b2216a72ab7a0dc64
sungjin122517 <sungjin122517@naver.com>
2024-07-02 22:49:54 +0800

    [FEAT] 로그아웃 구현

--- src/main/java/com/igdy/igeodaeyeo/global/security/AuthController.java
+++ src/main/java/com/igdy/igeodaeyeo/global/security/AuthController.java
@@ -1,8 +1,15 @@
 package com.igdy.igeodaeyeo.global.security;
 
+import com.igdy.igeodaeyeo.global.entity.ApiResponse;
+import com.igdy.igeodaeyeo.global.jwt.TokenResponse;
+import com.igdy.igeodaeyeo.global.jwt.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +19,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("auth")
 public class AuthController {
 
+    private final TokenService tokenService;
+
     @GetMapping("/success")
-    public ResponseEntity<String> success(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<TokenResponse>> success(HttpServletRequest request) {
         // get parameter code accessToken
         String accessToken = request.getParameter("accessToken");
-        return ResponseEntity.ok(accessToken);
+
+        TokenResponse tokenResponse = TokenResponse.builder()
+                .accessToken(accessToken)
+                .build();
+
+        ApiResponse<TokenResponse> apiResponse = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "로그인 성공",
+                tokenResponse
+        );
+
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<ApiResponse<Void>> logout(@AuthenticationPrincipal UserDetails userDetails) {
+        tokenService.deleteRefreshToken(userDetails.getUsername());
+
+        ApiResponse<Void> apiResponse = new ApiResponse<>(
+                HttpStatus.OK.value(),
+                "로그아웃 성공",
+                null
+        );
+
+        return ResponseEntity.ok(apiResponse);
     }
 }
